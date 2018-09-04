82780a37b491be924d0644c4598f2710d49d9442
han-seung-pyo <36193411+han-seung-pyo@users.noreply.github.com>
2018-09-04 22:36:01 +0900

    Update inverse transform method.py

--- inverse transform method.py
+++ inverse transform method.py
@@ -71,3 +71,48 @@ plt.show()
 print('-'*50)
 print('count of acceptance is %d'%(count_acceptance))
 print('-'*50)
+
+#%%
+#Box- Muller Method
+'''box-muller method algorism
+z1, z2 are independent standard normal random numbers
+R:반지름, P(R<=x) = 1 -np.e(-x/2)
+algorism
+1. r = -2 * np.log(U1)
+2. V = 2 * np.pi * U2
+3. z1 = np.sqrt(r) * np.cos(v), z2 = 2* np.sqrt(r) * np.sin(v)
+'''
+def box_muller(size):
+    data = pd.DataFrame({'R':-2*np.log(np.random.uniform(size = size)),'v' : 2*np.pi * np.random.uniform(size = size)})
+    data['z1'] = np.sqrt(data['R']) * np.cos(data['v'])
+    data['z2'] = np.sqrt(data['R']) * np.sin(data['v'])
+    return data
+bm = box_muller(1000000)
+plt.hist(bm.ix[:,'z1':'z2'].values.flatten(), bins = frequency2 ,rwidth = 0.5);
+plt.show()
+#Marsaglia and Bray Method(1964)
+'''
+--> 과거 cos, sin값을 계산하기 힘들어서 개발된 방법론.
+Algorism
+1. u1 = 2u1 -1
+2. u2 = 2u2 -1
+3. x = u1^2 +u2^2 <=1 ? accept : reject
+4. y = np.sqrt(-2*np.log(x)/x)    /// 여기서 x는 3번에서 accept된 x 이렇게 하면 uniform을 한번더 generate할 필요가 없다. accept된 x가  uniform하므로
+5. z1 = u1 * y, z2 = u2 * y
+'''
+def marsaglia_bray(size):
+    data = pd.DataFrame({'U1':2 * np.random.uniform(size = size)-1 , 'U2' :2 * np.random.uniform(size = size) -1 })
+    data['x'] = data['U1']**2 + data['U2']**2
+    data['y'] = pd.Series()
+    for i in range(len(data)):
+        if  data['x'][i] <=1:
+            data['y'][i] = np.sqrt((-2*np.log(data['x'][i])/data['x'][i]))
+        else:
+            data['y'][i] = 0
+    data['z1'] = data['U1'] * data['y']
+    data['z2'] = data['U2'] * data['y']
+    return data
+mb = marsaglia_bray(10000)
+
+plt.hist(mb.ix[:,'z1':'z2'].values.flatten(), bins = frequency2 ,rwidth = 0.5);
+plt.show()
