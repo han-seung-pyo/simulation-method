52f41cb9389b102a7d8462401642ecdbdba757e5
han-seung-pyo <36193411+han-seung-pyo@users.noreply.github.com>
2018-09-10 14:27:45 +0900

    Update inverse transform method.py

--- inverse transform method.py
+++ inverse transform method.py
@@ -3,7 +3,7 @@
 Created on Tue Aug 28 21:05:37 2018
 
 @author: 한승표
-날짜: 2018.08.28~30
+날짜: 2018.08.28~30, 9.3
 과목: 시뮬레이션 방법론
 pricing 방법론 중 numerical method에는 3가지가 있다. Lattice(tree model), FDM, monte carlo simuation
 
@@ -28,51 +28,25 @@ import matplotlib.pyplot as plt
 import scipy.stats
 from scipy.stats import norm
 from math import gamma
-
+import timeit
 # uniform method
-size = (1000,1000)
-frequency = np.arange(0.1,1,0.1);
-frequency2 = np.arange(-4,5,1);
 def inverse(size):
     uniform = np.random.uniform(size = size)
     norminv = norm.ppf(uniform)
     return uniform, norminv
-uniform, norminv = inverse(size)
-
-plt.hist(uniform.flatten(), bins = frequency,rwidth=0.5);
-plt.show();
-plt.hist(norminv.flatten(), bins=frequency2,  rwidth=0.5);
-plt.show();
-
 #Accepntance-Rejection Method
-alpha1 = 3;
-alpha2 = 2;
-size = 10000
 def accepntance(alpha1,alpha2,size):
     beta = (gamma(alpha1)*gamma(alpha2))/ gamma(alpha1+alpha2);
     max_x = (alpha1-1)/(alpha1+alpha2-2)
     c = (max_x**(alpha1-1)*(1-max_x)**(alpha2-1))/beta
-    u1 = pd.DataFrame(np.random.uniform(size = (size,1)))
-    u2 = pd.DataFrame(np.random.uniform(size = (size,1)))
-    fu = pd.DataFrame(((u1[0]**(alpha1-1))*(1-u1[0])**(alpha2-1))/beta/c)
-    fu['acceptance'] = pd.Series()
-    for i in range(len(fu)):
-        if fu.iloc[:,0][i] >= u2.iloc[:,0][i]:
-            fu['acceptance'][i] = u1[0][i]
-        else: 
-            fu['acceptance'][i] = 0
-    return fu
+    u1 = pd.DataFrame(np.random.uniform(size = (size,1)),columns=['u1'])
+    u2 = pd.DataFrame(np.random.uniform(size = (size,1)),columns=['u2'])
+    U = pd.concat([u1,u2],axis=1)
+    U['fu'] = ((u1**(alpha1-1))*(1-u1)**(alpha2-1))/beta/c
+    U['acceptance'] = U.apply(lambda row: row['u1'] if row['fu'] >row['u2'] else 0, axis=1)
+    return U
 
-x = accepntance(alpha1,alpha2,size)
-count_acceptance = np.count_nonzero(x.iloc[:,1])
 
-plt.hist(x['acceptance'], bins = frequency,rwidth=0.5);
-plt.show()
-print('-'*50)
-print('count of acceptance is %d'%(count_acceptance))
-print('-'*50)
-
-#%%
 #Box- Muller Method
 '''box-muller method algorism
 z1, z2 are independent standard normal random numbers
@@ -87,9 +61,6 @@ def box_muller(size):
     data['z1'] = np.sqrt(data['R']) * np.cos(data['v'])
     data['z2'] = np.sqrt(data['R']) * np.sin(data['v'])
     return data
-bm = box_muller(1000000)
-plt.hist(bm.ix[:,'z1':'z2'].values.flatten(), bins = frequency2 ,rwidth = 0.5);
-plt.show()
 #Marsaglia and Bray Method(1964)
 '''
 --> 과거 cos, sin값을 계산하기 힘들어서 개발된 방법론.
@@ -103,16 +74,51 @@ Algorism
 def marsaglia_bray(size):
     data = pd.DataFrame({'U1':2 * np.random.uniform(size = size)-1 , 'U2' :2 * np.random.uniform(size = size) -1 })
     data['x'] = data['U1']**2 + data['U2']**2
-    data['y'] = pd.Series()
-    for i in range(len(data)):
-        if  data['x'][i] <=1:
-            data['y'][i] = np.sqrt((-2*np.log(data['x'][i])/data['x'][i]))
-        else:
-            data['y'][i] = 0
+    data['y'] = data.apply(lambda row: np.sqrt((np.log(row['x'])*(-2))/row['x']) if row['x'] <=1 else 0, axis=1)
     data['z1'] = data['U1'] * data['y']
     data['z2'] = data['U2'] * data['y']
     return data
-mb = marsaglia_bray(10000)
-
-plt.hist(mb.ix[:,'z1':'z2'].values.flatten(), bins = frequency2 ,rwidth = 0.5);
+size = (1000,1000)
+alpha1 = 3;
+alpha2 = 2;
+size1 = 2500000
+size2=2500000
+frequency = np.arange(0.1,1,0.05);
+frequency2 = np.arange(-4,5,0.5);
+print('-'*50)
+print('-'*50)
+start = timeit.default_timer()
+uniform, norminv = inverse(size)
+end = timeit.default_timer()
+print('걸린 시간: %2f초'%(end - start))
+plt.hist(uniform.flatten(), bins = frequency,rwidth=0.5 ,label = 'Uniform' );
+plt.show();
+plt.hist(norminv.flatten(), bins=frequency2,  rwidth=0.5, label = 'normal' );
+plt.show();
+print('-'*50)
+print('-'*50)
+start = timeit.default_timer()
+x = accepntance(alpha1,alpha2,size1)
+end = timeit.default_timer()
+print('걸린 시간: %2f초'%(end - start))
+plt.hist(x['acceptance'], bins = frequency,rwidth=0.5, label = 'Acceptance and Rejection');
 plt.show()
+count_acceptance = np.count_nonzero(x.iloc[:,-1])
+print('count of acceptance is %d'%(count_acceptance))
+print('-'*50)
+print('-'*50)
+start = timeit.default_timer()
+bm = box_muller(size2)
+end = timeit.default_timer()
+print('걸린 시간: %2f초'%(end - start))
+plt.hist(bm.ix[:,'z1':'z2'].values.flatten(), bins = frequency2 ,rwidth = 0.5, label = 'Box_Muller Method');
+plt.show()
+print('-'*50)
+print('-'*50)
+start = timeit.default_timer()
+mb = marsaglia_bray(size1)
+end = timeit.default_timer()
+print('걸린 시간: %2f초'%(end - start))
+plt.hist(mb.ix[:,'z1':'z2'].values.flatten(), bins = frequency2 ,rwidth = 0.5, label='Marsaglia_Bray Method');
+plt.show()
+
