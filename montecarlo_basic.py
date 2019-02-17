ef238122c34c279a8e8064ebafa8e08f219ff637
han-seung-pyo <36193411+han-seung-pyo@users.noreply.github.com>
2019-02-17 09:24:58 +0900

    Update montecarlo_basic.py

--- montecarlo_basic.py
+++ montecarlo_basic.py
@@ -19,13 +19,13 @@ sim_num = 1000
 n_steps = 100
 
 #%%
-def Montecarlo_sim(s,T,r,sigma,sim_num,n_steps):
+def Montecarlo_sim(s,T,r,q,sigma,sim_num,n_steps):
     delta_t = T/n_steps
     z_matrix = np.random.standard_normal(size =(sim_num,n_steps))
     st_matrix = np.zeros((sim_num,n_steps))
-    st_matrix[:,0] = s0
+    st_matrix[:,0] = s
     for i in range(n_steps-1):
-        st_matrix[:,i+1] = st_matrix[:,i]*np.exp((r-0.5*sigma**2)*delta_t + sigma*np.sqrt(delta_t)*z_matrix[:,i])
+        st_matrix[:,i+1] = st_matrix[:,i]*np.exp((r-q-0.5*sigma**2)*delta_t + sigma*np.sqrt(delta_t)*z_matrix[:,i])
     
     return st_matrix
 
